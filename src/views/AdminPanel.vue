b22164c216f8552465297a6120291817b949bebc
Leszek Janczewski <leszek.janczewski@gmail.com>
2020-11-02 21:34:36 +0100

    dodanie przyciskow do panelu administracyjnego. closed #6

--- src/views/AdminPanel.vue
+++ src/views/AdminPanel.vue
@@ -1,15 +1,23 @@
 <template>
-  <div>
-    Admin Panel
-  </div>
+  <v-row class="admin-buttons">
+    <v-btn class="admin-button">Linie autobusowe</v-btn>
+    <v-btn class="admin-button">Przystanki</v-btn>
+    <v-btn class="admin-button">Godziny odjazdów</v-btn>
+  </v-row>
 </template>
 
 <script>
 export default {
   name: 'AdminPanel',
-
-  data: () => ({
-
-  })
+  data: () => ({})
 }
 </script>
+
+<style lang="less" scoped>
+.admin-buttons {
+  margin-top: 30px;
+}
+    .admin-button {
+      margin: 5px;
+    }
+</style>
